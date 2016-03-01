65e8b4c32ef82f6dc4679d4edfd4e32b123e4ea3
Dan Vuletici <dan.vuletici@busymachines.com>
2016-03-01 09:58:06 +0200

    pre jQ
    implementation of all elements

---
+++ js/zigninnovations.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['start', 'about_us', 'about_us', 'about_us', 'expertise', 'projects', 'contact'],
+    navigation: false,
+    menu: "navigation",
+    responsiveWidth: 900,
+    scrollBar:true,
+    // autoScrolling: false,
+		fitToSection: false,
+    normalScrollElements: '#section2, #section1',
+
+  });
+
+  $('.bottomBar .btn').click(function(e) {
+				e.preventDefault();
+				$.fn.fullpage.moveTo(2);
+  });
+
+
+
+});
+
+  $(window).scroll(function() {
+    // console.log($(this).scrollTop());
+
+    if( $(this).scrollTop() > 35 ) {
+      $("#navigation").addClass("nav-scrolled");
+    } else {
+      $("#navigation").removeClass("nav-scrolled");
+    }
+  });
+
+  // $("#navigation").scroll(function (event) {
+  //     var scroll = $(window).scrollTop();
+  //     console.log(scroll);
+  //     // Do something
+  // });
