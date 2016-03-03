85f9fab614a9a87cf11a2853ba6ea5ba9b51bf02
Cristi Filip <cristif3@gmail.com>
2016-03-03 16:45:48 +0200

    added map marker & coords

--- js/zigninnovations.js
+++ js/zigninnovations.js
@@ -85,7 +85,7 @@ $( document ).ready(function() {
   // When the window has finished loading create our google map below
   google.maps.event.addDomListener(window, 'load', init);
   google.maps.event.addDomListener(window, 'resize', function() {
-      map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+      map.setCenter(new google.maps.LatLng(52.0327419, 5.1015501));
   });
 
   function init() {
@@ -96,7 +96,7 @@ $( document ).ready(function() {
           zoom: 15,
 
           // The latitude and longitude to center the map (always required)
-          center: new google.maps.LatLng(40.6700, -73.9400), // New York
+          center: new google.maps.LatLng(52.0327419, 5.1015501), // New York
 
           // Disables the default Google Maps UI components
           disableDefaultUI: true,
@@ -223,8 +223,8 @@ $( document ).ready(function() {
       map = new google.maps.Map(mapElement, mapOptions);
 
       // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-      var image = 'img/map-marker.png';
-      var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+      var image = '../img/map-marker.png';
+      var myLatLng = new google.maps.LatLng(52.0327419, 5.1015501);
       var beachMarker = new google.maps.Marker({
           position: myLatLng,
           map: map,
